2f0bd1effc70c613ab4ba7fca414fd232e5b72cd
Connor Hynes <hynes24@uw.edu>
2022-07-10 18:19:47 -0700

    gui update

--- gui.py
+++ gui.py
@@ -1,16 +1,118 @@
 import sys
-from PyQt5 import QtWidgets
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QComboBox, QApplication, QWidget, QVBoxLayout, QLabel
 
-app = QtWidgets.QApplication(sys.argv)
-windows = QtWidgets.QWidget()
+# CONSTANTS
+year = ['2022', '2021', '2020', '2019', '2018', '2017']
+location = ['Austria']
+session = ['FP1','FP2', 'FP3', 'Sprint Qualifying', 'Qualifying', 'Race']
+driver_name = ['VER', 'LEC', 'SAI', 'HAM', 'RUS', 'MAG', 'MSC']
+analysis_type = ['Lap Time', 'Fastest Lap']
 
-windows.resize(1000, 500)
-windows.move(100, 100)
+class MainWindow(QWidget):
 
-windows.setWindowTitle('Formula 1 Analytics')
-windows.setWindowIcon(QtGui.QIcon('formula/img/f1.png'))
-windows.show()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.UIComponents()
 
-sys.exit(app.exec_())
+    def initUI(self):
+        self.setFixedSize(560, 280)
+        self.move(100, 100)
+        self.setWindowTitle('Formula 1 Analytics')
+        self.setWindowIcon(QtGui.QIcon('formula/img/f1.png'))
+
+    def UIComponents(self):
+        layout = QVBoxLayout()
+
+        self.drop_year = QComboBox()
+        self.drop_grand_prix = QComboBox()
+        self.drop_session = QComboBox()
+        self.drop_driver1 = QComboBox()
+        self.drop_driver2 = QComboBox()
+        self.drop_analysis = QComboBox()
+
+        label_year = QLabel()
+        label_prix = QLabel()
+        label_session = QLabel()
+        label_d1 = QLabel()
+        label_d2 = QLabel()
+        label_analysis = QLabel()
+
+        label_year.setText('Year:') 
+        label_prix.setText('Grand Prix Location:') 
+        label_session.setText('Session:') 
+        label_d1.setText('Driver 1:') 
+        label_d2.setText('Driver 2:') 
+        label_analysis.setText('Analysis Type:')
+
+        self.drop_year.addItems(year)
+        self.drop_grand_prix.addItems(location)
+        self.drop_session.addItems(session)
+        self.drop_driver1.addItems(driver_name)
+        self.drop_driver2.addItems(driver_name)
+        self.drop_analysis.addItems(analysis_type)
+
+        layout.addWidget(label_year)
+        layout.addWidget(self.drop_year)
+        layout.addWidget(label_prix)
+        layout.addWidget(self.drop_grand_prix)
+        layout.addWidget(label_session)
+        layout.addWidget(self.drop_session)
+        layout.addWidget(label_d1)
+        layout.addWidget(self.drop_driver1)
+        layout.addWidget(label_d2)
+        layout.addWidget(self.drop_driver2)
+        layout.addWidget(label_analysis)
+        layout.addWidget(self.drop_analysis)
+
+
+ 
+        self.drop_year.activated.connect(self.current_text)
+        self.drop_grand_prix.activated.connect(self.current_text)
+        self.drop_session.activated.connect(self.current_text)
+        self.drop_driver1.activated.connect(self.current_text)
+        self.drop_driver2.activated.connect(self.current_text)
+        self.drop_analysis.activated.connect(self.current_text)
+
+
+        self.setLayout(layout)
+        self.show()
+
+        
+    # listens for change in inputs, responds to each change with updated list of desired inputs.
+    def current_text(self):
+        input_data = []
+        text = self.drop_year.currentText()
+        input_data.append(text)
+        text = self.drop_grand_prix.currentText()
+        input_data.append(text)
+        text = self.drop_session.currentText()
+        input_data.append(text)
+        text = self.drop_driver1.currentText()
+        input_data.append(text)
+        text = self.drop_driver2.currentText()
+        input_data.append(text)
+        text = self.drop_analysis.currentText()
+        input_data.append(text)
+        print(input_data)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
+#w.setLayout(layout)
+
+#w.show()
+
+#sys.exit(app.exec_())
 
--- main.py
+++ main.py
@@ -6,55 +6,52 @@ from fastf1 import api
 
 import pandas as pd
 import numpy as np
-import datetime
 
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import figure
 
 # enables cache, allows storage of race data locally
 ff1.Cache.enable_cache('formula/cache')
-
-# patches matplotlib for time delta support! wow!
+# patches matplotlib for time delta support
 ff1.plotting.setup_mpl(mpl_timedelta_support = True, color_scheme = 'fastf1')
 
-year = 2022
-grand_prix = 'Austria' # 'FP1', 'FP2', 'FP3', 'Q', 'S', 'SQ', 'R'
-session = 'R'
 
 
-race = ff1.get_session(year, grand_prix, session)
-race.load()
+YEAR = 2022
+GRAND_PRIX = 'Austria' 
+SESSION = 'R' # 'FP1', 'FP2', 'FP3', 'Q', 'S', 'SQ', 'R'
 
-driver1 = 'RIC' # ['HAM' 'VER' 'BOT' 'LEC' 'OCO' 'SAI' 'RIC' 'PER' 'NOR' 'ALO' 'STR' 'GAS'
-                #'VET' 'GIO' 'RUS' 'TSU' 'RAI' 'MSC' 'LAT' 'MAZ']
-driver2 = 'NOR'
+driver1 = 'LEC'
+driver2 = 'SAI'
 
-laps_d1 = race.laps.pick_driver(driver1)
-laps_d2 = race.laps.pick_driver(driver2)
+def get_race_data():
+    race = ff1.get_session(YEAR, GRAND_PRIX, SESSION)
+    race.load()
+    return race
 
-print(laps_d1['LapTime'])
-print(laps_d1.columns)
+def plot_laptime(race):
+    laps_d1 = race.laps.pick_driver(driver1)
+    laps_d2 = race.laps.pick_driver(driver2)
 
-# plotting
+    color1 = ff1.plotting.driver_color(driver1)
+    color2 = ff1.plotting.driver_color(driver2)
 
-color1 = ff1.plotting.team_color('FER')
-color2 = ff1.plotting.team_color('RBR')
+    fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()
+    ax.plot(laps_d1['LapNumber'], laps_d1['LapTime'], color = color1, label = driver1)
+    ax.plot(laps_d2['LapNumber'], laps_d2['LapTime'], color = color2, label = driver2)
 
+    ax.set_xlabel('Lap Number')
+    ax.set_ylabel('Lap Time')
 
-ax.plot(laps_d1['LapNumber'], laps_d1['LapTime'], color = color1, label = driver1)
-ax.plot(laps_d2['LapNumber'], laps_d2['LapTime'], color = color2, label = driver2)
+    ax.legend()
+    plt.suptitle(f"Lap Time Comparison \n" f"{race.event['EventName']} {race.event.year}")
 
-ax.set_xlabel('Lap Number')
-ax.set_ylabel('Lap Time')
-
-ax.legend()
-plt.suptitle(f"Lap Time Comparison \n" f"{race.event['EventName']} {race.event.year}")
-
-plt.show()
-
-
+    plt.show()
 
 
+def main():
+    race = get_race_data()
+    plot_laptime(race)
 
+main()
